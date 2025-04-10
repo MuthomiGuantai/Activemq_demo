2e41f28d3aab3da8a265de4997196f27a4e15496
Guantai Bruce Muthomi <guantai.muthomi@MDGF01BE6B.local>
2025-04-10 12:04:09 +0300

    added timestamp,multiple destinations and dlq for failed messages

--- src/main/java/com/bruceycode/activemq_demo/component/MessageRoute.java
+++ src/main/java/com/bruceycode/activemq_demo/component/MessageRoute.java
@@ -11,8 +11,17 @@ public class MessageRoute extends RouteBuilder {
     public void configure() throws Exception {
         from("direct:sendMessage")
                 .routeId("sendMessageRoute")
+                .process(exchange -> {
+                    String body = exchange.getIn().getBody(String.class);
+                    exchange.getIn().setBody(body + " [Sent at " + System.currentTimeMillis() + "]");
+                })
                 .log(LoggingLevel.INFO, "Sending message: ${body}")
-                .to("jms:queue:demo-queue");
+                .choice()
+                .when(simple("${body} contains 'error'"))
+                .to("jms:queue:error-queue")
+                .otherwise()
+                .to("jms:queue:demo-queue")
+                .end();
 
         from("jms:queue:demo-queue")
                 .routeId("processMessageRoute")
@@ -23,5 +32,10 @@ public class MessageRoute extends RouteBuilder {
                         .retryAttemptedLogLevel(LoggingLevel.WARN))
                 .bean("messageProcessor", "processMessage")
                 .log(LoggingLevel.INFO, "Successfully processed: ${body}");
+
+        from("jms:queue:dead-letter-queue")
+                .routeId("dlqRoute")
+                .log(LoggingLevel.ERROR, "Dead letter message: ${body}")
+                .bean("messageConsumer", "receiveMessage");
     }
 }
